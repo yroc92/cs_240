1afafd5e958011c1184ff71b43b3bc85a1dc10fd
Cory Cooper <coopercory92@gmail.com>
2017-03-24 12:07:56 -0600

    Adding new tests

---
+++ myserver/src/test/java/server/TestMain.java
@@ -0,0 +1,25 @@
+package test.java.server;
+
+import main.java.server.Database;
+
+import java.sql.SQLException;
+
+class TestMain {
+
+    public TestMain() {}
+
+    public static void main(String[] args) {
+        Database.init();
+        DAOTest daoTest = new DAOTest();
+        try {
+            daoTest.testAddOneOfEachModelToDatabase();
+            daoTest.testGeneratePerson();
+            daoTest.testGenerateEvents();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+
+}
